5ba16833377ebbba626f261222904c37bb043b05
dhafinrh <dhafin.rizqullah@upi.edu>
2023-05-24 15:54:24 +0700

    Aset sementara + Tilemap

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+//Controller untuk pergerakan, animasi, dan serangan 
+public class PlayerController : MonoBehaviour
+{
+    void OnMove(InputValue movementValue)
+    {
+
+    }
+}
